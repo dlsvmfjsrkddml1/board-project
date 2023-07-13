d9d9de4775eff70b5f193466e4da24d89b5c3e9e
sojourn88 <sojourn88@gmail.com>
2023-07-13 10:34:45 +0900

    #17 - 시큐리티 설정을 모든 요청에 인증이 열리도록 수정
    지금은 테스트 개발 단계이고, 아직 인증 기능 구현 단계에 이르지 않았으므로, 모든 인증 설정을 오픈한다. 그리고 폼 로그인을 활성화해서 로그인 뷰가 그려지게끔 함. 최근 스프링 부트 2.7의 설정 방법을 활용

---
+++ board-project/src/main/java/yong/boardproject/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package yong.boardproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.servlet.http.HttpSession;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin().and()
+                .build();
+    }
+}
--- board-project/src/test/java/yong/boardproject/controller/ArticleControllerTest.java
+++ board-project/src/test/java/yong/boardproject/controller/ArticleControllerTest.java
@@ -5,13 +5,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import yong.boardproject.config.SecurityConfig;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class) // 해당 컨트롤러만 테스트를 함.
 class ArticleControllerTest {
 
